d9f61e1ad6b069511a1327373020e11c52bb2902
Resul ÇELİK <r.celik@mf.karaelmas.edu.tr>
2023-02-28 20:19:39 +0300

    Deneme

--- test.go
+++ test.go
@@ -5,15 +5,15 @@ import (
 )
 
 func main() {
-	//CutVideo("test.mp4", "test_cut.mp4", "00:00:40", "00:00:50")
-	StartStream("https://demiroren.daioncdn.net/kanald/kanald_480p.m3u8?&sid=56t16au2wka1&app=da2109ea-5dfe-4107-89ab-23593336ed61&ce=3", "test_%Y%m%d%H%M%S.mp4", "30")
+	CutVideo("test.mp4", "test_cut.mp4", "00:00:40", "00:00:50")
+	//StartStream("https://demiroren.daioncdn.net/kanald/kanald_480p.m3u8?&sid=56t16au2wka1&app=da2109ea-5dfe-4107-89ab-23593336ed61&ce=3", "test_%Y%m%d%H%M%S.mp4", "30")
 }
 
 func CutVideo(sorucePath string, destPath string, startTime string, endTime string) {
-	ffmpeg.Input(sorucePath, ffmpeg.KwArgs{"ss": startTime, "to": endTime, "async": "1", "strict": "-2"}).Output(destPath).OverWriteOutput().ErrorToStdOut().Run()
+	ffmpeg.Input(sorucePath, ffmpeg.KwArgs{"ss": startTime, "to": endTime, "async": "1", "strict": "-2"}).Output(destPath).OverWriteOutput().Run()
 
 }
 
 func StartStream(source string, dest string, segmentTime string) {
-	ffmpeg.Input(source).Output(dest, ffmpeg.KwArgs{"b:v": "1M", "f": "segment", "segment_time": segmentTime, "reset_timestamps": "1", "strftime": "1"}).ErrorToStdOut().Run()
+	ffmpeg.Input(source).Output(dest, ffmpeg.KwArgs{"b:v": "1M", "f": "segment", "segment_time": segmentTime, "reset_timestamps": "1", "strftime": "1"}).Run()
 }
